dca24f1f6d46dab716139d17b9d8d5efcd7217a6
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2016-12-13 13:31:01 -0500

    fixed bug

--- app/controllers/evaluate_controller.rb
+++ app/controllers/evaluate_controller.rb
@@ -8,9 +8,12 @@ class EvaluateController < ApplicationController
 
   # POST /evaluate
   def run
+    parameters = params[:parameters] || {}
+    parameters = parameters.empty? ? {} : JSON.parse(params[:parameters]) if parameters.is_a?(String)
+
     b = binding
-    parameters = params[:parameters].is_a?(String) ? JSON.parse(params[:parameters]) : params[:parameters]
     parameters.each { |k, v| b.local_variable_set(k, v) }
+
     eval params[:code], b
   rescue Exception => ex
     render :text => ex, status: 500
